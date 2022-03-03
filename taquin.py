4c079062eac8943437b68f99ab9d0b1583f0ae6c
ilya2s <ilyass.elouazzani97@outlook.fr>
2022-03-02 22:26:51 -0500

    implemented function inversions(tab, x)

--- taquin.py
+++ taquin.py
@@ -1,1 +1,11 @@
-import tuiles
+def inversions(tab, x):
+    
+    index = tab.index(x) + 1
+    
+    counter = 0
+    
+    for i, element in range(index, len(tab)):
+        
+        if tab[element] < x and tab[element] != 0:
+            counter += 1
+
